52cb59e3bfee15b9405744e4e6a904188a5a0ca1
hossain-mo <hossainfcih@gmail.com>
2022-10-12 01:42:33 +0200

    revert

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./src/app');
 const { appPort } = require('./config/app-config');
 
 const boot = () => {
-  app.listen(vvvvc appPort, () => {
+  app.listen(appPort, () => {
     console.log(`server running on port ${appPort}`);
   });
 };
